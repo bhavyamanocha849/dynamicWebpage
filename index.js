caf8802f37400a7f96bcd305b4792b0d6c4e2f87
bhavyamanocha849 <bhavyamanocha849@gmail.com>
2019-07-21 09:58:29 +0530

    post added

--- index.js
+++ index.js
@@ -65,6 +65,5 @@ var server = http.createServer(function(req, res) {
   }
 });
 var port =process.env.PORT||80;
-server.listen(3000);
-
+server.listen(port);
 console.log("Server is listening at port 3000");
